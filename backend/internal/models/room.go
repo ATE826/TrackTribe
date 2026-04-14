9c1aba7b513515fb81f00ce5e1f0581080913875
Anton Tseysnovskiy <antontseysnovskiy@gmail.com>
2026-04-14 14:01:27 +0300

    Models: romm & user

--- backend/internal/models/room.go
+++ backend/internal/models/room.go
@@ -1,1 +1,26 @@
 package models
+
+import "gorm.io/gorm"
+
+type RoomStatus string
+
+const (
+	RoomActive RoomStatus = "active"
+	RoomEnded  RoomStatus = "ended"
+)
+
+type RoomPlan string
+
+const (
+	PlanFree    RoomPlan = "free"
+	PlanPremium RoomPlan = "premium"
+)
+
+type Room struct {
+	gorm.Model
+
+	Code     string     `gorm:"not null"`
+	Status   RoomStatus `gorm:"not null; default:'active'"`
+	Plan     RoomPlan   `gorm:"not null; default:'free'"`
+	MaxUsers int        `gorm:"default:10"`
+}
